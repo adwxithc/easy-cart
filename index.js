d0b37c840dfbb807c319e8bd5ec5f95114f2d199
adwxithc <adwaithjanardhanan0@gmail.com>
2023-10-06 17:23:29 +0530

    first setup of the project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+
+
+
+const PORT=process.env.PORT||3000
+
+app.listen(PORT,()=>{
+    console.log(`app runs at : http://localhost:${PORT}`)
+})

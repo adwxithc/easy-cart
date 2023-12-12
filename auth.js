69f07085db6cba8b12c62e089e6faefc3d6c9379
adwxithc <adwaithjanardhanan0@gmail.com>
2023-12-13 01:31:47 +0530

    google auth error solving

--- auth.js
+++ auth.js
@@ -6,10 +6,10 @@ const userHelpers=require('./helperMethods/userHelpers')
 
 
 passport.use(new GoogleStrategy({
-    clientID:     process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `/auth/google/callback`,
-    passReqToCallback   : true
+    clientID:process.env.GOOGLE_CLIENT_ID,
+    clientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL:'/auth/google/callback',
+    passReqToCallback:true
   },  
   function(request, accessToken, refreshToken, profile, done) {
     
